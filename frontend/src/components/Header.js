1a046b2be7f865866ac0c6c61530cedba6b0d0a2
Xinyi Liao <78093922+lisaliao0817@users.noreply.github.com>
2023-10-30 20:43:02 -0300

    Deleted the icon in the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -28,7 +28,6 @@ export default function Header() {
           <div className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <h1 className="text-2xl font-bold text-indigo-800 mt-2">ToDo</h1>
-            {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
           </div>
         </div>
 
